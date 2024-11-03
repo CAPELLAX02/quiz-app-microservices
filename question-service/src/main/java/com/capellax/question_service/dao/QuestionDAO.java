0660d7aa1a6d4c6bfc9c916c90ea13c2f8e9a0fe
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-03 21:44:18 +0300

    Spring Boot project initialization: Question Service

---
+++ question-service/src/main/java/com/capellax/question_service/dao/QuestionDAO.java
@@ -0,0 +1,25 @@
+package com.capellax.question_service.dao;
+
+import com.capellax.question_service.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDAO extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategoryIgnoreCase(String category);
+
+    @Query(
+            value = "SELECT * FROM question_service_db.public.question q WHERE q.category=:category ORDER BY RANDOM() LIMIT :numQ",
+            nativeQuery = true
+    )
+    List<Question> findRandomQuestionsByCategory(
+            String category,
+            int numQ
+    );
+
+}
---
+++ question-service/src/main/java/com/capellax/question_service/service/QuestionService.java
@@ -0,0 +1,50 @@
+package com.capellax.question_service.service;
+
+import com.capellax.question_service.dao.QuestionDAO;
+import com.capellax.question_service.model.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+    private final QuestionDAO questionDAO;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDAO.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(
+            String category
+    ) {
+        try {
+            return new ResponseEntity<>(questionDAO.findByCategoryIgnoreCase(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(
+            Question question
+    ) {
+        try {
+            questionDAO.save(question);
+            return new ResponseEntity<>("Question added successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+}
