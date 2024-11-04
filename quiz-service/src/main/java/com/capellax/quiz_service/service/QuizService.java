86eb9c4d1408d39b7b55703222a1e8e8cbde779b
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-04 17:15:42 +0300

    Other endpoint methods completed in Quiz service

--- quiz-service/src/main/java/com/capellax/quiz_service/service/QuizService.java
+++ quiz-service/src/main/java/com/capellax/quiz_service/service/QuizService.java
@@ -41,50 +41,21 @@ public class QuizService {
         return new ResponseEntity<>("Quiz created successfully.", HttpStatus.CREATED);
     }
 
-    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-//        Optional<Quiz> quizOptional = quizDAO.findById(id);
-//        if (quizOptional.isEmpty()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//
-//        List<Question> questionsFromDB = quizOptional.get().getQuestions();
-//        if (questionsFromDB.isEmpty()) {
-//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//        }
-//
-//        List<QuestionWrapper> questionForUser = questionsFromDB.stream()
-//                .map(question -> new QuestionWrapper(
-//                        question.getId(),
-//                        question.getQuestionTitle(),
-//                        question.getOption1(),
-//                        question.getOption2(),
-//                        question.getOption3(),
-//                        question.getOption4()
-//                ))
-//                .toList();
-//
-//        return new ResponseEntity<>(questionForUser, HttpStatus.OK);
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(
+            Integer id
+    ) {
+        Quiz quiz = quizDAO.findById(id).get();
+        List<Integer> questionIds = quiz.getQuestionIds();
+        ResponseEntity<List<QuestionWrapper>> questions = quizInterface.getQuestionsFromId(questionIds);
+        return questions;
     }
 
-    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
-//        Optional<Quiz> quizOptional = quizDAO.findById(id);
-//        if (quizOptional.isEmpty()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//
-//        Quiz quiz = quizOptional.get();
-//        List<Question> questions = quiz.getQuestions();
-//
-        int rightAnswerCounter = 0;
-//
-//        for (int i = 0; i < responses.size() && i < questions.size(); i++) {
-//            if (responses.get(i).getResponse().equals(questions.get(i).getRightAnswer())) {
-//                rightAnswerCounter++;
-//            }
-//        }
-//
-        return new ResponseEntity<>(rightAnswerCounter, HttpStatus.OK);
+    public ResponseEntity<Integer> calculateResult(
+            Integer id,
+            List<Response> responses
+    ) {
+        ResponseEntity<Integer> score = quizInterface.getScore(responses);
+        return score;
     }
 
 }
